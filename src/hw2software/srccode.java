8758e869a899f30a831964b80c208d765462b4c1
Tala99 <hethnawi.tala@gmail.com>
2020-03-01 12:23:25 +0200

    init

---
+++ src/hw2software/srccode.java
@@ -0,0 +1,9 @@
+package hw2software;
+
+public class srccode {
+public static int add(int a, int b) {
+		
+		return a+b;
+	}
+	
+}
---
+++ src/testingpk/testcodeTest.java
@@ -0,0 +1,18 @@
+package testingpk;
+
+
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import hw2software.srccode;
+
+
+class testcodeTest {
+@Test
+	 public void tt(){
+	        assertEquals(srccode.add(4,5), 9);
+	    }
+
+}
